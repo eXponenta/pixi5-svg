79bb623e3413ba2c0bc2c8f71062db6eb151895c
Konstantin <rondo.devil@gmail.com>
2019-09-01 20:28:48 +0300

    Codegen

--- src/paletted/palettedgraphicsrender.js
+++ src/paletted/palettedgraphicsrender.js
@@ -1,55 +1,75 @@
 import { pixi } from "./../extends";
 
-class PalettedShaderGenerator
-{
-    /**
-     * @param {string} vertexSrc - Vertex shader
-     * @param {string} fragTemplate - Fragment shader template
-     */
-    constructor(vertexSrc, fragTemplate)
-    {
-        this.vertexSrc = vertexSrc;
-        this.fragTemplate = fragTemplate;
-        this.programCache = {};
-        this.defaultGroupCache = {};
-    }
+class PalettedShaderGenerator {
+	/**
+	 * @param {string} vertexSrc - Vertex shader
+	 * @param {string} fragTemplate - Fragment shader template
+	 */
+	constructor(vertexSrc, fragTemplate) {
+		this.vertexSrc = vertexSrc;
+		this.fragTemplate = fragTemplate;
+		this.programCache = {};
+		this.defaultGroupCache = {};
+	}
 
-    generateShader(maxTextures)
-    {
-        if (!this.programCache[maxTextures])
-        {
-            const sampleValues = new Int32Array(maxTextures);
-			const sampleSizeValues = new Float32Array(maxTextures * 2);
+	generateShader(maxTextures) {
+		if (!this.programCache[maxTextures]) {
+			const sampleValues = new Int32Array(maxTextures);
+			const sampleSizeValues = new Float32Array(maxTextures);
 
-            for (let i = 0; i < maxTextures; i++)
-            {
-                sampleValues[i] = i;
-            }
+			for (let i = 0; i < maxTextures; i++) {
+				sampleValues[i] = i;
+			}
 
-            this.defaultGroupCache[maxTextures] = pixi.UniformGroup.from(
-				{ 
+			this.defaultGroupCache[maxTextures] = pixi.UniformGroup.from(
+				{
 					uSamplers: sampleValues,
-					uSamplersSize : sampleSizeValues
-				 }, true);
-            
+					uSamplersSize: sampleSizeValues
+				},
+				true
+			);
+
 			let fragmentSrc = this.fragTemplate;
 			let vertexSrc = this.vertexSrc;
 
 			vertexSrc = vertexSrc.replace(/%count%/gi, `${maxTextures}`);
 			fragmentSrc = fragmentSrc.replace(/%count%/gi, `${maxTextures}`);
-            
+			vertexSrc = vertexSrc.replace(/%data_loop%/gi, this.generateSampleSrc(maxTextures));
+
 			this.programCache[maxTextures] = new pixi.Program(vertexSrc, fragmentSrc);
 		}
+		const uniforms = {
+			tint: new Float32Array([1, 1, 1, 1]),
+			translationMatrix: new pixi.Matrix(),
+			uSamplersSize: new Float32Array(maxTextures),
+			default: this.defaultGroupCache[maxTextures]
+		};
 
-        const uniforms = {
-            tint: new Float32Array([1, 1, 1, 1]),
-            translationMatrix: new pixi.Matrix(),
-			uSamplersSize : new Float32Array(maxTextures),
-            default: this.defaultGroupCache[maxTextures],
-        };
+		return new pixi.Shader(this.programCache[maxTextures], uniforms);
+	}
 
-        return new pixi.Shader(this.programCache[maxTextures], uniforms);
-    }
+	generateSampleSrc(maxTextures) {
+		let src = "\n\n";
+		for (let i = 0; i < maxTextures; i++) {
+            
+            if (i > 0) {
+				src += "\nelse ";
+			}
+            
+            if (i < maxTextures - 1) {
+				src += `if(aTextureId < ${i}.5)`;
+			}
+            
+            src += `{
+                vStrokeColor = texture2D(uSamplers[${i}], pS);
+                vStrokeData.yz = texture2D(uSamplers[${i}], pD).xy;
+                vFillColor = texture2D(uSamplers[${i}], p);
+            }`;
+		}
+		src += "\n";
+		src += "\n";
+		return src;
+	}
 }
 
 const vertex = `
@@ -83,17 +103,13 @@ const vertex = `
 		float size = uSamplersSize[textureId];
 		float hsize = size * 4.;
 
-		vec2 pos = 0.5 + vec2(4. * mod(shapeId, 1. / hsize), floor(shapeId * hsize));
-		pos *= size;
-
 		vStrokeData = vec4(aColor.r, 0., 0., 0.);
 		
-		if(aColor.r > 0.) {
-			vStrokeColor = texture2D(uSamplers[textureId], pos + vec2(1. * size ,0.));
-			vStrokeData.yz = texture2D(uSamplers[textureId], pos + vec2(2. * size ,0.)).xy;
-		} else {
-			vFillColor = texture2D(uSamplers[textureId], pos);
-		}
+		vec2 p = size * ( 0.5 + vec2(4. * mod(shapeId, 1. / hsize), floor(shapeId * hsize)));
+        vec2 pS = p + vec2(1. * size, 0.);
+        vec2 pD = p + vec2(2. * size, 0.);
+        
+		%data_loop%
 
 		vTextureCoord = aTextureCoord;
 		vTextureId = aTextureId;
@@ -127,16 +143,11 @@ const fragment = `
 	}
 `;
 
-const {
-	premultiplyBlendMode
-} = pixi.utils;
+const { premultiplyBlendMode } = pixi.utils;
 
-const {
-	settings, BaseTexture
-} = pixi;
+const { settings, BaseTexture } = pixi;
 
 export class PalettedGraphicsRenderer extends pixi.AbstractBatchRenderer {
-	
 	constructor(renderer) {
 		super(renderer);
 
@@ -144,167 +155,150 @@ export class PalettedGraphicsRenderer extends pixi.AbstractBatchRenderer {
 		this.geometryClass = pixi.BatchGeometry;
 		this.vertexSize = 6;
 	}
-	
-    flush()
-    {
-        if (this._vertexCount === 0)
-        {
-            return;
-        }
 
-        const attributeBuffer = this.getAttributeBuffer(this._vertexCount);
-        const indexBuffer = this.getIndexBuffer(this._indexCount);
-        
-        const {
-            _bufferedElements: elements,
-            _drawCalls: drawCalls,
-            MAX_TEXTURES,
-            _packedGeometries: packedGeometries,
-            vertexSize,
-        } = this;
+	flush() {
+		if (this._vertexCount === 0) {
+			return;
+		}
 
-        const touch = this.renderer.textureGC.count;
+		const attributeBuffer = this.getAttributeBuffer(this._vertexCount);
+		const indexBuffer = this.getIndexBuffer(this._indexCount);
 
-        let index = 0;
-        let _indexCount = 0;
+		const {
+			_bufferedElements: elements,
+			_drawCalls: drawCalls,
+			MAX_TEXTURES,
+			_packedGeometries: packedGeometries,
+			vertexSize
+		} = this;
 
-        let nextTexture;
-        let currentTexture;
-        let textureCount = 0;
+		const touch = this.renderer.textureGC.count;
 
-        let currentGroup = drawCalls[0];
-        let groupCount = 0;
+		let index = 0;
+		let _indexCount = 0;
 
-        let blendMode = -1;// blend-mode of previous element/sprite/object!
+		let nextTexture;
+		let currentTexture;
+		let textureCount = 0;
 
-        currentGroup.textureCount = 0;
-        currentGroup.start = 0;
-        currentGroup.blend = blendMode;
+		let currentGroup = drawCalls[0];
+		let groupCount = 0;
 
-        let TICK = ++BaseTexture._globalBatch;
-        let i;
+		let blendMode = -1; // blend-mode of previous element/sprite/object!
 
-        for (i = 0; i < this._bufferSize; ++i)
-        {
-            const sprite = elements[i];
+		currentGroup.textureCount = 0;
+		currentGroup.start = 0;
+		currentGroup.blend = blendMode;
 
-            elements[i] = null;
-            nextTexture = sprite._texture.baseTexture;
+		let TICK = ++BaseTexture._globalBatch;
+		let i;
 
-            const spriteBlendMode = premultiplyBlendMode[
-                nextTexture.premultiplyAlpha ? 1 : 0][sprite.blendMode];
+		for (i = 0; i < this._bufferSize; ++i) {
+			const sprite = elements[i];
 
-            if (blendMode !== spriteBlendMode)
-            {
-                blendMode = spriteBlendMode;
+			elements[i] = null;
+			nextTexture = sprite._texture.baseTexture;
 
-                // force the batch to break!
-                currentTexture = null;
-                textureCount = MAX_TEXTURES;
-                TICK++;
-            }
+			const spriteBlendMode = premultiplyBlendMode[nextTexture.premultiplyAlpha ? 1 : 0][sprite.blendMode];
 
-            if (currentTexture !== nextTexture)
-            {
-                currentTexture = nextTexture;
+			if (blendMode !== spriteBlendMode) {
+				blendMode = spriteBlendMode;
 
-                if (nextTexture._batchEnabled !== TICK)
-                {
-                    if (textureCount === MAX_TEXTURES)
-                    {
-                        TICK++;
+				// force the batch to break!
+				currentTexture = null;
+				textureCount = MAX_TEXTURES;
+				TICK++;
+			}
 
-                        textureCount = 0;
+			if (currentTexture !== nextTexture) {
+				currentTexture = nextTexture;
 
-                        currentGroup.size = _indexCount - currentGroup.start;
+				if (nextTexture._batchEnabled !== TICK) {
+					if (textureCount === MAX_TEXTURES) {
+						TICK++;
 
-                        currentGroup = drawCalls[groupCount++];
-                        currentGroup.textureCount = 0;
-                        currentGroup.blend = blendMode;
-                        currentGroup.start = _indexCount;
-                    }
+						textureCount = 0;
 
-                    nextTexture.touched = touch;
-                    nextTexture._batchEnabled = TICK;
-                    nextTexture._id = textureCount;
+						currentGroup.size = _indexCount - currentGroup.start;
 
-                    currentGroup.textures[currentGroup.textureCount++] = nextTexture;
-                    textureCount++;
-                }
-            }
+						currentGroup = drawCalls[groupCount++];
+						currentGroup.textureCount = 0;
+						currentGroup.blend = blendMode;
+						currentGroup.start = _indexCount;
+					}
 
-            this.packInterleavedGeometry(sprite, attributeBuffer,
-                indexBuffer, index, _indexCount);
+					nextTexture.touched = touch;
+					nextTexture._batchEnabled = TICK;
+					nextTexture._id = textureCount;
 
-            // push a graphics..
-            index += (sprite.vertexData.length / 2) * vertexSize;
-            _indexCount += sprite.indices.length;
-        }
+					currentGroup.textures[currentGroup.textureCount++] = nextTexture;
+					textureCount++;
+				}
+			}
 
-        BaseTexture._globalBatch = TICK;
-        currentGroup.size = _indexCount - currentGroup.start;
+			this.packInterleavedGeometry(sprite, attributeBuffer, indexBuffer, index, _indexCount);
 
-        if (!settings.CAN_UPLOAD_SAME_BUFFER)
-        { /* Usually on iOS devices, where the browser doesn't
+			// push a graphics..
+			index += (sprite.vertexData.length / 2) * vertexSize;
+			_indexCount += sprite.indices.length;
+		}
+
+		BaseTexture._globalBatch = TICK;
+		currentGroup.size = _indexCount - currentGroup.start;
+
+		if (!settings.CAN_UPLOAD_SAME_BUFFER) {
+			/* Usually on iOS devices, where the browser doesn't
             like uploads to the same buffer in a single frame. */
-            if (this._packedGeometryPoolSize <= this._flushId)
-            {
-                this._packedGeometryPoolSize++;
-                packedGeometries[this._flushId] = new (this.geometryClass)();
-            }
+			if (this._packedGeometryPoolSize <= this._flushId) {
+				this._packedGeometryPoolSize++;
+				packedGeometries[this._flushId] = new this.geometryClass();
+			}
 
-            packedGeometries[this._flushId]._buffer.update(attributeBuffer.rawBinaryData, 0);
-            packedGeometries[this._flushId]._indexBuffer.update(indexBuffer, 0);
+			packedGeometries[this._flushId]._buffer.update(attributeBuffer.rawBinaryData, 0);
+			packedGeometries[this._flushId]._indexBuffer.update(indexBuffer, 0);
 
-            this.renderer.geometry.bind(packedGeometries[this._flushId]);
-            this.renderer.geometry.updateBuffers();
-            this._flushId++;
-        }
-        else
-        {
-            // lets use the faster option, always use buffer number 0
-            packedGeometries[this._flushId]._buffer.update(attributeBuffer.rawBinaryData, 0);
-            packedGeometries[this._flushId]._indexBuffer.update(indexBuffer, 0);
+			this.renderer.geometry.bind(packedGeometries[this._flushId]);
+			this.renderer.geometry.updateBuffers();
+			this._flushId++;
+		} else {
+			// lets use the faster option, always use buffer number 0
+			packedGeometries[this._flushId]._buffer.update(attributeBuffer.rawBinaryData, 0);
+			packedGeometries[this._flushId]._indexBuffer.update(indexBuffer, 0);
 
-            this.renderer.geometry.updateBuffers();
-        }
+			this.renderer.geometry.updateBuffers();
+		}
 
 		this._processDraw(drawCalls, groupCount);
-        // reset elements for the next flush
-        this._bufferSize = 0;
-        this._vertexCount = 0;
-        this._indexCount = 0;
-    }
+		// reset elements for the next flush
+		this._bufferSize = 0;
+		this._vertexCount = 0;
+		this._indexCount = 0;
+	}
 
 	_processDraw(drawCalls, groupCount) {
-		
 		const gl = this.renderer.gl;
 		const textureSystem = this.renderer.texture;
-        const stateSystem = this.renderer.state;
+		const stateSystem = this.renderer.state;
 		const shaderSystem = this.renderer.shader;
 		const shader = this._shader;
 		const uSamplersSize = this._shader.uniforms.uSamplersSize;
 
 		// Upload textures and do the draw calls
-        for (let i = 0; i < groupCount; i++)
-        {
-            const group = drawCalls[i];
-            const groupTextureCount = group.textureCount;
+		for (let i = 0; i < groupCount; i++) {
+			const group = drawCalls[i];
+			const groupTextureCount = group.textureCount;
 
-            for (let j = 0; j < groupTextureCount; j++)
-            {
+			for (let j = 0; j < groupTextureCount; j++) {
 				const base = group.textures[j];
 
-				uSamplersSize[j] = 1. / base.width;
-                textureSystem.bind(base, j);
-                group.textures[j] = null;
-            }
+				uSamplersSize[j] = 1 / base.width;
+				textureSystem.bind(base, j);
+				group.textures[j] = null;
+			}
 
-            debugger
-            shaderSystem.syncUniformGroup(shader.uniformGroup);
-            stateSystem.setBlendMode(group.blend);
-            gl.drawElements(group.type, group.size, gl.UNSIGNED_SHORT, group.start * 2);
-        }
+			shaderSystem.syncUniformGroup(shader.uniformGroup);
+			stateSystem.setBlendMode(group.blend);
+			gl.drawElements(group.type, group.size, gl.UNSIGNED_SHORT, group.start * 2);
+		}
 	}
 }
-
